72e5462eca5f2faad476ea89c7f5bc909b634b38
paunchymochi <paunchymochi@gmail.com>
2022-03-31 02:35:52 +0800

    [cell.Cells] Implement Cells

--- sudokupy/cell.py
+++ sudokupy/cell.py
@@ -1,3 +1,4 @@
+from typing import Union, List
 
 class Cell:
     __slots__ = ['_row', '_column', '_value']
@@ -49,3 +50,41 @@ class Cell:
         if value not in range(0, 10):
             raise ValueError('cell value must be an integer between 0 and 9')
 
+
+class Cells:
+    def __init__(self, cells:Union['Cells', List['Cells'], List[List['Cells']]]=None, _sliced=False):
+        self._sliced = _sliced
+        if cells is None:
+            self._make_default_cells()
+        else:
+            if isinstance(cells, Cell):
+                self._cells = [[cells]]
+            elif isinstance(cells[0], Cell):
+                self._cells = [cells]
+            else:
+                self._cells = cells
+    
+    def __str__(self):
+        return '\n'.join([' '.join([str(item.value) for item in row]) for row in self._cells])
+    
+    def __repr__(self):
+        return '<Cells>\n' + '\n'.join([' '.join([str(item.value) for item in row]) for row in self._cells])
+
+    def __getitem__(self, key):
+        if type(key) is tuple:
+            cells = self._cells[key[0]]
+            if type(cells[0]) is list:
+                return Cells([row[key[1]] for row in cells], _sliced=True)
+            else:
+                return Cells(cells[key[1]], _sliced=True)
+        else:
+            return Cells(self._cells[key], _sliced=True)
+
+    def _make_default_cells(self):
+        cells = []
+        for r in range(9):
+            row = []
+            for c in range(9):
+                row.append(Cell(r, c, 0))
+            cells.append(row)
+        self._cells = cells
