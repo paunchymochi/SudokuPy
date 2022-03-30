073fe40079e6d96c812104c67c4acc791cc18c3e
paunchymochi <paunchymochi@gmail.com>
2022-03-31 00:56:27 +0800

    [cell.Cell] Implement get properties

--- sudokupy/cell.py
+++ sudokupy/cell.py
@@ -4,3 +4,15 @@ class Cell:
         self._row = row
         self._column = column
         self._value = value
+    
+    @property
+    def row(self):
+        return self._row
+    
+    @property
+    def column(self):
+        return self._column
+    
+    @property
+    def value(self):
+        return self._value
