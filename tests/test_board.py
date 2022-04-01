e718b7f59e00c5beaf2eb3b9ec96997c1d0202e0
paunchymochi <paunchymochi@gmail.com>
2022-04-02 07:39:50 +0800

    [test/board.TestBoard] Implement test_row & test_col

--- tests/test_board.py
+++ tests/test_board.py
@@ -3,10 +3,18 @@ import pytest
 
 class TestBoard:
     def test_row(self):
-        raise NotImplementedError
+        b = Board()
+        row = b.row[1]
+        assert len(row) == 9
+        assert row.topleft_row == 1
+        assert row.topleft_column == 0
     
     def test_col(self):
-        raise NotImplementedError
+        b = Board()
+        col = b.col[1]
+        assert len(col) == 9
+        assert col.topleft_column == 1
+        assert col.topleft_row == 0
 
     def test_box(self):
         raise NotImplementedError
