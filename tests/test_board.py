3bc5ac3118d80759be14100582fe5a353cff3ecb
paunchymochi <paunchymochi@gmail.com>
2022-04-02 03:22:38 +0800

    [test/board] Create test_board.py

---
+++ tests/test_board.py
@@ -0,0 +1,12 @@
+from ..sudokupy.board import Board
+import pytest
+
+class TestBoard:
+    def test_row(self):
+        raise NotImplementedError
+    
+    def test_col(self):
+        raise NotImplementedError
+
+    def test_box(self):
+        raise NotImplementedError
