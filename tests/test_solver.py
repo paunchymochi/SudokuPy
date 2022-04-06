8eaaac8f85aeba2b9a9eb1ecaf1a229abff58786
paunchymochi <paunchymochi@gmail.com>
2022-04-07 02:27:35 +0800

    [test/solver.TestCompanionDeducer] Add test_single_companion

---
+++ tests/test_solver.py
@@ -0,0 +1,36 @@
+import pytest
+import sys
+sys.path.append('..')
+from sudokupy.cell import Cells
+from sudokupy.board import Board
+from sudokupy.solver import CompanionDeducer, LineBoxDeducer, ValueDeducer, Deducer
+
+@pytest.fixture
+def blank_board():
+    board = Board()
+    for cell in board.cells.flatten():
+        cell.set_candidates([])
+    return board
+
+class TestCompanionDeducer:
+    def test_single_companion(self):
+        d = CompanionDeducer()
+        board = Board()
+        board.cell[0, 1].set_candidates(5)
+
+        d.deduce(board.row[1])
+        assert len(d.operations) == 8
+
+        board.cell[0, 2].remove_candidates(1)
+        d.clear_operations()
+        d.deduce(board.row[1])
+        assert len(d.operations) == 7
+
+class TestLineBoxDeducer:
+    pass
+
+class TestValueDeducer:
+    pass
+
+class TestDeducer:
+    pass
