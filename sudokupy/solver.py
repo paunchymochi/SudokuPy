f3115c54577d2da4a6c1bbc463892af4c0d315ad
paunchymochi <paunchymochi@gmail.com>
2022-04-08 02:56:09 +0800

    [solver.CompanionDeducer] Refactor class

--- sudokupy/solver.py
+++ sudokupy/solver.py
@@ -192,39 +192,38 @@ class CompanionDeducer(_BaseDeducer):
 
     def __init__(self):
         super().__init__()
-        self._sliced_cells:Cells=None
 
     def deduce(self, sliced_cells:Cells, max_companion_count:int=3):
-        self._sliced_cells = sliced_cells
         self._operations_in_current_slice = False
 
-        max_level = self._get_max_level(self._sliced_cells, max_companion_count)
+        max_level = self._get_max_level(sliced_cells, max_companion_count)
         companions = {}
         level = 1
 
         companions[0] = [None]
 
+        flattened_sliced_cells = sliced_cells.flatten()
 
         while level <= max_level and not self._operations_in_current_slice:
-            companions[level] = self._make_companions(self._sliced_cells, companions[level-1])
+            companions[level] = self._make_companions(flattened_sliced_cells, companions[level-1])
             level += 1
         
-    def _make_companions(self, sliced_cells:Cells, companions:List[_Companion]=None) -> List[_Companion]:
+    def _make_companions(self, flattened_sliced_cells:List[Cell], companions:List[_Companion]=None) -> List[_Companion]:
         if companions is None:
             companions = [None]
         new_companions = []
         for other_companion in companions:
-            for cell in sliced_cells.flatten():
+            for cell in flattened_sliced_cells:
                 companion = _Companion(cell, other_companion)
                 if companion not in new_companions:
                     if not companion.skip:
                         new_companions.append(companion)
                     if companion.valid:
-                        self._make_new_operations(companion, sliced_cells)
+                        self._make_new_operations(companion, flattened_sliced_cells)
         return new_companions
     
-    def _make_new_operations(self, companion:_Companion, sliced_cells:Cells):
-        for cell in sliced_cells.flatten():
+    def _make_new_operations(self, companion:_Companion, flattened_sliced_cells:List[Cell]):
+        for cell in flattened_sliced_cells:
             if cell not in companion.cells:
                 if any([candidate in companion.companion for candidate in cell.candidates]):
                     self._operations_in_current_slice = True
