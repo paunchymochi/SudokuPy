7490289baeb1d17e956d0e6150951a4da0512193
paunchymochi <paunchymochi@gmail.com>
2022-04-09 01:45:54 +0800

    [file.File] Implement set_folder

--- sudokupy/file.py
+++ sudokupy/file.py
@@ -5,17 +5,20 @@ from sudokupy.board import Board
 from sudokupy.cell import Cell
 
 class File:
-    def __init__(self):
-        pass
+    def __init__(self, folder:str=None):
+        self.set_folder(folder)
     
     def read_csv(self, filename:str):
         pass
 
-    def set_folder(self):
-        pass
+    def set_folder(self, folder:str):
+        self._folder = folder
 
     def get_folder(self) -> Path:
-        return Path('boards')
+        folder = self._folder
+        if folder is None:
+            folder = 'boards'
+        return Path(folder)
 
     def get_path(self, filename:str) -> Path:
         folder = self.get_folder()
