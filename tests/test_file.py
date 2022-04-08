41553b9e61a8abe9d53d90c9ed8bb39fdc4d30db
paunchymochi <paunchymochi@gmail.com>
2022-04-09 02:03:00 +0800

    [test/file.TestFile] Implement test_read_csv

--- tests/test_file.py
+++ tests/test_file.py
@@ -12,3 +12,8 @@ class TestFile:
         file = File()
         path = file.get_path('easy01.csv')
         assert path.exists() == True
+    
+    def test_read_csv(self):
+        file = File()
+        board = file.read_csv('easy01.csv')
+        assert board.row[0].get_values(flatten=True) == [7, 0, 4, 9, 0, 0, 5, 6, 8]
