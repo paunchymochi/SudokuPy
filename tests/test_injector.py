3d2e9795070a2f15ce9e71a9305e455917d6e855
paunchymochi <paunchymochi@gmail.com>
2022-04-11 17:02:43 +0800

    [test/injector.TestInjector] Create TestInjector

---
+++ tests/test_injector.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append('..')
+from sudokupy.board import Board
+from sudokupy.injector import Injector
+import pytest
+
+class TestInjector:
+    def test_inject_one_value(self):
+        board = Board()
+        j = Injector(board.cells)
+        j.inject()
+        assert board.cell[0, 0].flatten()[0].value in list(range(1, 10))
+        assert len(j._backups) == 1
+        assert j._guesses == 1
+    
+    def test_inject_no_solution(self):
+        board = Board()
+        j = Injector(board.cells)
+
+        board.cell[0, 0].candidates = []
+
+        with pytest.raises(ValueError):
+            j.inject()
+    
+    def test_inject_no_solution_2(self):
+        b = Board()
+        j = Injector(b.cells)
+        b.cell[0, 3].candidates = []
+
+        for i in range(9):
+            j.inject()
+        
+        with pytest.raises(ValueError):
+            j.inject()
+
