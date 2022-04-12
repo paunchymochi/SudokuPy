6938cd62badbc36d634edd48d8fbcc8c7fb3bd12
paunchymochi <paunchymochi@gmail.com>
2022-04-12 15:25:49 +0800

    [cell.Cells] Fix set_candidates

--- sudokupy/cell.py
+++ sudokupy/cell.py
@@ -362,8 +362,16 @@ class Cells:
         if type(values) is int:
             values = [values]
         
-        for i, cell in enumerate(self.flatten()):
-            cell.set_candidates(values[i])
+        if len(values) == 0:
+            for cell in self.flatten():
+                cell.set_candidates(values)
+        elif type(values[0]) in [list, tuple]:
+            for i, cell in enumerate(self.flatten()):
+                cell.set_candidates(values[i])
+        else:
+            for cell in self.flatten():
+                cell.set_candidates(values)
+        
 
     def _make_default_cells(self):
         row_count = 9
