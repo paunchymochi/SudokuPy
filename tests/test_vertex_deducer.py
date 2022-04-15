7408d8032612f9ad76942da95f111aff0650b3a5
paunchymochi <paunchymochi@gmail.com>
2022-04-16 06:16:11 +0800

    [TestVertexPair] Implement test_topleft_cell

--- tests/test_vertex_deducer.py
+++ tests/test_vertex_deducer.py
@@ -28,3 +28,12 @@ class TestVertexPair:
         c2 = Cell(5, 0, 0)
         p = VertexPair(2, Cell(0, 0, 0), c1, c2)
         assert p.cells == (c1, c2)
+    
+    def test_topleft_cell(self):
+        for i in range(9):
+            c = Cell(0, i, 0)
+            
+            p = VertexPair(1, c, Cell(3, i, 0), Cell(7, i, 0))
+            assert p.topleft_cell == c
+
+
