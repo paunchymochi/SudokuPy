5eb3a99ffb2e1ad88474e49efc768dec15456706
paunchymochi <paunchymochi@gmail.com>
2022-04-16 23:14:31 +0800

    Update VertexPair constructor arguments

--- tests/test_vertex_deducer.py
+++ tests/test_vertex_deducer.py
@@ -6,35 +6,33 @@ from sudokupy.cell import Cell, Cells
 
 class TestVertexPair:
     def test_is_row_pair(self):
-        p = VertexPair(1, Cell(1, 0, 0), Cell(1, 2, 0), Cell(1, 5, 0))
+        p = VertexPair(1, Cell(1, 2, 0), Cell(1, 5, 0))
         assert p.is_row_pair() == True
-        p = VertexPair(1, Cell(0, 5), Cell(0, 5), Cell(8, 5))
+        p = VertexPair(1, Cell(0, 5), Cell(8, 5))
         assert p.is_row_pair() == False
     
     def test_eq(self):
-        p = VertexPair(1, Cell(0, 0, 0), Cell(5, 0, 0), Cell(8, 0, 0))
-        p2 = VertexPair(2, Cell(0, 0, 0), Cell(5, 0, 0), Cell(8, 0, 0))
-        p3 = VertexPair(1, Cell(0, 0, 0), Cell(1, 0, 0), Cell(2, 0, 0))
+        p = VertexPair(1, Cell(5, 0, 0), Cell(8, 0, 0))
+        p2 = VertexPair(2, Cell(5, 0, 0), Cell(8, 0, 0))
+        p3 = VertexPair(1, Cell(1, 0, 0), Cell(2, 0, 0))
         assert p != p2
         assert p == p3
     
     def test_candidate(self):
         for i in range(1, 10):
-            p = VertexPair(i, Cell(0, 0, 0), Cell(4, 0, 0), Cell(5, 0, 0))
+            p = VertexPair(i, Cell(4, 0, 0), Cell(5, 0, 0))
             assert p.candidate == i
     
     def test_cells(self):
         c1 = Cell(3, 0, 0)
         c2 = Cell(5, 0, 0)
-        p = VertexPair(2, Cell(0, 0, 0), c1, c2)
+        p = VertexPair(2, c1, c2)
         assert p.cells == (c1, c2)
     
-    def test_topleft_cell(self):
-        for i in range(9):
-            c = Cell(0, i, 0)
-            
-            p = VertexPair(1, c, Cell(3, i, 0), Cell(7, i, 0))
-            assert p.topleft_cell == c
+    # def test_topleft_cell(self):
+    #     for i in range(9):
+    #         p = VertexPair(1, Cell(3, i, 0), Cell(7, i, 0))
+    #         assert p.topleft_cell == c
 
     def test_row_col(self):
         cases = [
@@ -42,36 +40,36 @@ class TestVertexPair:
             [[0, 4], [0, 8]]
         ]
         for case in cases:
-            p = VertexPair(1, Cell(0, 0, 0), Cell(case[0][0], case[0][1]), Cell(case[1][0], case[1][1]))
+            p = VertexPair(1, Cell(case[0][0], case[0][1]), Cell(case[1][0], case[1][1]))
             assert p.rows == [case[0][0], case[1][0]]
             assert p.cols == [case[0][1], case[1][1]]
     
     def test_vertex_rowcol(self):
-        p = VertexPair(1, Cell(0, 0, 0), Cell(0, 5, 0), Cell(0, 8, 0))
+        p = VertexPair(1, Cell(0, 5, 0), Cell(0, 8, 0))
         assert p.vertex_row == 0
         assert p.vertex_cols == [5, 8]
 
-        p = VertexPair(1, Cell(0, 0, 0), Cell(1, 0, 0), Cell(4, 0, 0))
+        p = VertexPair(1, Cell(1, 0, 0), Cell(4, 0, 0))
         assert p.vertex_row == 0
         assert p.vertex_cols == [1, 4]
     
     def test_is_row_pair(self):
-        p = VertexPair(1, Cell(0, 0, 0), Cell(0, 5, 0), Cell(0, 8, 0))
+        p = VertexPair(1, Cell(0, 5, 0), Cell(0, 8, 0))
         assert p.is_row_pair == True
-        p = VertexPair(1, Cell(0, 0, 0), Cell(1, 0, 0), Cell(4, 0, 0))
+        p = VertexPair(1, Cell(1, 0, 0), Cell(4, 0, 0))
         assert p.is_row_pair == False
 
     def test_invalid_inputs(self):
         with pytest.raises(ValueError):
-            p = VertexPair(1, Cell(0, 0, 0), Cell(1, 1, 0), Cell(0, 5, 0))
+            p = VertexPair(1, Cell(1, 1, 0), Cell(0, 5, 0))
         
         with pytest.raises(ValueError):
-            p = VertexPair(1, Cell(1, 1, 1), Cell(1, 5, 1), Cell(1, 7, 1))
+            p = VertexPair(1, Cell(1, 5, 1), Cell(1, 7, 1))
 
 class TestVertexCouple:
     def test_valid_couple(self):
-        p1 = VertexPair(1, Cell(0, 0, 0), Cell(0, 4, 0), Cell(0, 7, 0))
-        p2 = VertexPair(1, Cell(5, 0, 0), Cell(5, 4, 0), Cell(5, 7, 0))
+        p1 = VertexPair(1, Cell(0, 4, 0), Cell(0, 7, 0))
+        p2 = VertexPair(1, Cell(5, 4, 0), Cell(5, 7, 0))
         c1 = VertexCouple([p1])
         assert c1.pairs == [p1]
         assert c1.discard == False
@@ -88,7 +86,7 @@ class TestVertexCouple:
         assert c2.valid == True
     
     def test_invalid_parameter(self):
-        p = VertexPair(1, Cell(0, 0, 0), Cell(0, 4, 0), Cell(0, 7, 0))
+        p = VertexPair(1, Cell(0, 4, 0), Cell(0, 7, 0))
         with pytest.raises(TypeError):
             c = VertexCouple(p)
         
