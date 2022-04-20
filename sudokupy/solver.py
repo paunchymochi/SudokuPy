d772434cd302c5cf05ab733e3371d4af34e770dd
paunchymochi <paunchymochi@gmail.com>
2022-04-21 02:51:44 +0800

    [Solver] Implement solved_board & unsolved_board

--- sudokupy/solver.py
+++ sudokupy/solver.py
@@ -10,14 +10,18 @@ from typing import List
 
 class Solver:
     def __init__(self, board:Board=None):
-        self._board:Board = None
+        self._unsolved_board:Board = None
+        self._solved_board:Board = None
         self._deducer:Deducer = None
         self._injector:Injector = None
         self.set_board(board)
 
     @property
-    def board(self) -> Board:
-        return self._board
+    def solved_board(self) -> Board:
+        return self._solved_board
+    @property
+    def unsolved_board(self) -> Board:
+        return self._unsolved_board
     
     @property
     def transactions(self) -> List[Transaction]:
@@ -27,24 +31,28 @@ class Solver:
     def injections(self) -> List[_Injection]:
         return self._injector.injections
     
+    def __repr__(self):
+        return f'<Solver\nOriginal Board:\n{self._unsolved_board.cells.__str__()}\n\nSolved Board:\n{self._solved_board.cells.__str__()}\n>'
+
     def set_board(self, board:Board):
         if board is None:
             return
         if not isinstance(board, Board):
             raise TypeError('board must be an instance of Board')
-        self._board = board
-        self._deducer = Deducer(board.cells)
-        self._injector = Injector(board.cells)
+        self._unsolved_board = board.copy()
+        self._solved_board = board.copy()
+        self._deducer = Deducer(self._solved_board.cells)
+        self._injector = Injector(self._solved_board.cells)
     
     def solve(self, board:Board=None) -> Board:
         self.set_board(board)
         while not self._is_board_solved():
             self._deduce()
             self._inject()
-        return self._board
+        return self._solved_board
     
     def _is_board_solved(self) -> bool:
-        cells = self._board.cells.get_values(flatten=True)
+        cells = self._solved_board.cells.get_values(flatten=True)
         if 0 in cells:
             return False
         else:
