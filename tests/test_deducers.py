8701acacbdc19fd20c609d706c31460ee98570a0
paunchymochi <paunchymochi@gmail.com>
2022-04-20 19:16:37 +0800

    [TestLineBoxDeducer] Move to standalone file

--- tests/test_deducers.py
+++ tests/test_deducers.py
@@ -4,7 +4,6 @@ sys.path.append('..')
 from sudokupy.cell import Cells, Cell
 from sudokupy.board import Board
 from sudokupy.deducers.deducer_base import  Transaction, Transactions
-from sudokupy.deducers.linebox_deducer import LineBoxDeducer
 from sudokupy.deducers.value_deducer import ValueDeducer
 from sudokupy.deducers.vertex_deducer import VertexCoupleDeducer
 
@@ -119,49 +118,6 @@ class TestTransactions:
         result = t.get_transactions()
         assert type(result) is list
         assert len(result) == 1
-
-class TestLineBoxDeducer:
-    def test_repr(self):
-        d = LineBoxDeducer(Board.cells)
-        assert 'LineBoxDeducer' in d.__repr__()
-
-    def test_row_linebox(self):
-        board = Board()
-        d = LineBoxDeducer(board.cells)
-        for cell in board.row[0].flatten():
-            cell.set_candidates([])
-        
-        board.cell[0, 0].set_candidates([1, 8, 9])
-        board.cell[0, 1].set_candidates([1, 3, 6, 8])
-        board.cell[0, 3].set_candidates([1, 4, 5, 6])
-        board.cell[0, 4].set_candidates([5, 6, 7, 9])
-        board.cell[0, 7].set_candidates([1, 2, 3, 5])
-        board.cell[0, 8].set_candidates([2, 4, 6, 7, 9])
-
-        d.deduce(row=0)
-        assert len(d.transactions) == 12
-        for transaction in d.transactions:
-            assert transaction.candidates in [[2], [8]]
-        
-    def test_col_linebox(self):
-        board = Board()
-        d = LineBoxDeducer(board.cells)
-        for cell in board.col[4].flatten():
-            cell.set_candidates([])
-        
-        # remove 1 & 9 in box[0, 1]
-        board.cell[0, 4].set_candidates([1, 3, 7, 8])
-        board.cell[1, 4].set_candidates([1, 4, 7, 9])
-        board.cell[2, 4].set_candidates([2, 4, 7, 9])
-        board.cell[3, 4].set_candidates([2, 3, 4, 7])
-        board.cell[4, 4].set_candidates([3, 5, 6, 8])
-        board.cell[7, 4].set_candidates([2, 5, 7, 8])
-        board.cell[8, 4].set_candidates([4, 5, 6])
-
-        d.deduce(col=4)
-        assert len(d.transactions) == 6
-        for transaction in d.transactions:
-            assert transaction.candidates == [1, 9]
 
 class TestValueDeducer:
     def test_repr(self):
