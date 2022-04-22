7ad816b56b8e1955e3042d1f60b769b302353377
paunchymochi <paunchymochi@gmail.com>
2022-04-22 12:02:01 +0800

    [TestFile] Implement test_to_csv

--- tests/test_file.py
+++ tests/test_file.py
@@ -2,6 +2,7 @@ import pytest
 import sys
 sys.path.append('..')
 from sudokupy.file import File
+from sudokupy.cell import Cells
 
 class TestFile:
     def test_get_folder(self):
@@ -23,3 +24,18 @@ class TestFile:
         file = File()
         with pytest.raises(FileExistsError):
             board = file.read_csv('zzzzz.csv')
+    
+    def test_to_csv(self, tmpdir):
+        file = File(tmpdir)
+        cells = Cells()
+        filename = 'empty.csv'
+
+        result = file.to_csv(cells, filename)
+        assert result == [','.join(['0']*9) for _ in range(9)]
+        path = file.get_path(filename)
+        assert path.exists()
+
+        cells2 = file.read_csv(filename)
+        assert cells == cells2
+
+
